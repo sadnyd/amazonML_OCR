8932a414b3f6ea6b0cc7a5ef86ea6b542e685802
sadnyd <sadnyd@github.com>
2024-09-15 23:18:16 +0530

    updated parsing

--- src/constantsMod.py
+++ src/constantsMod.py
@@ -1,73 +1,68 @@
-# Define the mapping of units and their abbreviations
 unit_mapping = {
     'width': [
-        ('centimeter', 'cm'),
-        ('foot', 'ft'),
-        ('inch', 'in'),
-        ('metre', 'm'),  # Or use ('meter', 'm') for US spelling
-        ('millimeter', 'mm'),
-        ('yard', 'yd')
+        ['centimeter', 'cm'],
+        ['foot', 'ft', "'"],
+        ['inch', 'in', '"'],
+        ['metre', 'm'],  # Or use ['meter', 'm'] for US spelling
+        ['millimeter', 'mm'],
+        ['yard', 'yd']
     ],
     'depth': [
-        ('centimeter', 'cm'),
-        ('foot', 'ft'),
-        ('inch', 'in'),
-        ('metre', 'm'),
-        ('millimeter', 'mm'),
-        ('yard', 'yd')
+        ['centimeter', 'cm'],
+        ['foot', 'ft', "'"],
+        ['inch', 'in', '"'],
+        ['metre', 'm'],
+        ['millimeter', 'mm'],
+        ['yard', 'yd']
     ],
     'height': [
-        ('centimeter', 'cm'),
-        ('foot', 'ft'),
-        ('inch', 'in'),
-        ('metre', 'm'),
-        ('millimeter', 'mm'),
-        ('yard', 'yd')
+        ['centimeter', 'cm'],
+        ['foot', 'ft', "'"],
+        ['inch', 'in', '"'],
+        ['metre', 'm'],
+        ['millimeter', 'mm'],
+        ['yard', 'yd']
     ],
     'item_weight': [
-        ('gram', 'gm'),
-        ('kilogram', 'kg'),
-        ('microgram', 'µg'),
-        ('milligram', 'mg'),
-        ('ounce', 'oz'),
-        ('pound', 'lb'),
-        ('ton', 't')
+        ['gram', 'gm'],
+        ['kilogram', 'kg'],
+        ['microgram', 'µg','ug'],
+        ['milligram', 'mg'],
+        ['ounce', 'oz'],
+        ['pound', 'lb'],
+        ['ton', 't']
     ],
     'maximum_weight_recommendation': [
-        ('gram', 'gm'),
-        ('kilogram', 'kg'),
-        ('microgram', 'µg'),
-        ('milligram', 'mg'),
-        ('ounce', 'oz'),
-        ('pound', 'lb'),
-        ('ton', 't')
+        ['gram', 'gm'],
+        ['kilogram', 'kg'],
+        ['microgram', 'µg','mg'],
+        ['milligram', 'mg'],
+        ['ounce', 'oz'],
+        ['pound', 'lb'],
+        ['ton', 't']
     ],
     'voltage': [
-        ('kilovolt', 'kV'),
-        ('millivolt', 'mV'),
-        ('volt', 'V')
+        ['kilovolt', 'kv'],
+        ['millivolt', 'mv'],
+        ['volt', 'v']
     ],
     'wattage': [
-        ('kilowatt', 'kW'),
-        ('watt', 'W')
+        ['kilowatt', 'kw'],
+        ['watt', 'w']
     ],
     'item_volume': [
-        ('centilitre', 'cL'),
-        ('cubic foot', 'ft³','cfeet'),
-        ('cubic inch', 'in³','cinch'),
-        ('cup', 'cup'),
-        ('decilitre', 'dL'),
-        ('fluid ounce', 'fl oz'),
-        ('gallon', 'gal'),
-        ('imperial gallon', 'imp gal'),
-        ('litre', 'L'),
-        ('microlitre', 'µL'),
-        ('millilitre', 'mL'),
-        ('pint', 'pt'),
-        ('quart', 'qt')
+        ['centilitre', 'cL'],
+        ['cubic foot', 'ft3', 'cfeet'],
+        ['cubic inch', 'in3', 'cinch'],
+        ['cup', 'cup'],
+        ['decilitre', 'dL'],
+        ['fluid ounce', 'fl oz'],
+        ['gallon', 'gal'],
+        ['imperial gallon', 'imp gal'],
+        ['litre', 'L'],
+        ['microlitre', 'µL','ug'],
+        ['millilitre', 'mL'],
+        ['pint', 'pt'],
+        ['quart', 'qt']
     ]
 }
-
-# Example usage: printing out the mappings for 'width'
-for unit, abbrs in unit_mapping['width']:
-    print(f"{unit}: {abbrs}")
